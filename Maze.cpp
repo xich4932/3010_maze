d7ccc54a4555ad5ada5edb9025de8cb110c1526d
xich4932 <70279863+xich4932@users.noreply.github.com>
2021-09-19 20:46:56 -0600

    from mac

--- Maze.cpp
+++ Maze.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<vector>
+#include<cstdlib>
 #include"Maze.h"
+#include<time.h>
 using namespace std;
 
 Board::Board(){
@@ -39,17 +41,30 @@ vector<int> getDirection(int r, int c, int max_r, int max_c){
     }else if(r == max_r - 1){
         ret.erase(ret.begin()+1);
     }
-    if(r == 0) ret.push_back()
+    if(c == 0) ret.push_back(){
+        ret.erase(ret.begin());
+    }else if(c == max_c - 1){
+        ret.erase(ret.begin()+2);
+    }
+    return ret;
 }
 
 void generate(){
     visited[board_.get_cols()][board_.get_rows()];
-    vevtor<int> path;
-    path.push_back(0);    
-                            
-    
-    while(path[path.size()-1] != ){
-        
+    path.push_back(0);
+    int start_r = 0;
+    int start_c = 0;
+    int end_c = board_.get_cols() - 1;
+    int end_r = board_.get_rows() - 1;
+    while(start_r != end_r && start_c != end_c){
+        vevtor<int> direction = getDirection(0,0,board_.get_rows(), board_.get_cols());
+        srand((unsigned ) time(NULL));
+        int curr = rand()%direction.size();
+        start_c +=direction[curr][0];
+        start_r +=direction[curr][1];
+        direction.clear();
+
     }
+
 }
 
